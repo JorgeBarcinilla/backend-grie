68961e01752ec4a958bd573e190e9639105e314f
JORGE PALACIO <ctcdesarrollo4@hotmail.com>
2020-04-03 02:00:34 -0500

    Se corrige el modelo del item financiero en el modelo de campus

--- src/models/campus.js
+++ src/models/campus.js
@@ -1,29 +1,88 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
+const mongoose = require('mongoose')
+const {
+  Schema
+} = mongoose
 
 const CampusSchema = new Schema({
-    idSchool: {type: Schema.ObjectId, ref: "School"},
-    nombre: {type: String, required: true},
-    codDane : {type: String},
-    coordinador : {type: String},
-    cantEstudiantes : {type: String},
-    cantDocentes : {type: String},
-    datosNivelDirectivo : [{nombre: String, cargo:String, telefono:String, correo:String}],
-    municipio : {type: String},
-    barrio : {type: String},
-    direccion : {type: String},
-    telefono : {type: String},
-    correo : {type: String},
-    limiteNorte : {type: String},
-    limiteEste : {type: String},
-    limiteOeste : {type: String},
-    limiteSur : {type: String},
-    anioConstruccion: {type: String},
-    areaConstruida: {type: String},
-    areaLibre: {type: String},
-    archivosSoporte: {type: String},
-    edificios: [{numero: String, pisos: String, salones:[{cantidad: String, tipoSalon: String}]}],
-    jornadas: [{type: String}],
-});
+  idSchool: {
+    type: Schema.ObjectId,
+    ref: 'School'
+  },
+  nombre: {
+    type: String,
+    required: true
+  },
+  codDane: {
+    type: String
+  },
+  coordinador: {
+    type: String
+  },
+  cantEstudiantes: {
+    type: String
+  },
+  cantDocentes: {
+    type: String
+  },
+  datosNivelDirectivo: [{
+    nombre: String,
+    cargo: String,
+    telefono: String,
+    correo: String
+  }, ],
+  municipio: {
+    type: String
+  },
+  barrio: {
+    type: String
+  },
+  direccion: {
+    type: String
+  },
+  telefono: {
+    type: String
+  },
+  correo: {
+    type: String
+  },
+  limiteNorte: {
+    type: String
+  },
+  limiteEste: {
+    type: String
+  },
+  limiteOeste: {
+    type: String
+  },
+  limiteSur: {
+    type: String
+  },
+  anioConstruccion: {
+    type: String
+  },
+  areaConstruida: {
+    type: String
+  },
+  areaLibre: {
+    type: String
+  },
+  archivosSoporte: {
+    type: String
+  },
+  edificios: [{
+    numero: String,
+    pisos: String,
+    salones: [{
+      cantidad: String,
+      tipoSalon: String
+    }],
+  }, ],
+  jornadas: {
+    maniana: Boolean,
+    unica: Boolean,
+    tarde: Boolean,
+    noche: Boolean,
+  },
+})
 
-module.exports = mongoose.model('Campus', CampusSchema);
+module.exports = mongoose.model('Campus', CampusSchema)
