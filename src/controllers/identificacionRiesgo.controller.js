b5fc59031b277f889ef4482080fd498a915d6072
JORGE PALACIO <ctcdesarrollo4@hotmail.com>
2020-04-28 14:40:39 -0500

    ultimos cambios

--- src/controllers/identificacionRiesgo.controller.js
+++ src/controllers/identificacionRiesgo.controller.js
@@ -1,134 +1,143 @@
-const IdentificacionRiesgo = require("../models/identificacionRiesgo");
-const removeNullValues = require("../helpers/removeNullValues")
-const identificacionRiesgoCtrl = {};
+const IdentificacionRiesgo = require('../models/identificacionRiesgo')
+const removeNullValues = require('../helpers/removeNullValues')
+const identificacionRiesgoCtrl = {}
 
 identificacionRiesgoCtrl.getIdentificacionRiesgo = async (req, res) => {
-    const identificacionRiesgo = await IdentificacionRiesgo.find({
-        idCampus: req.params.idCampus
-    }, req.params.keys == 'undefined' ? null : req.params.keys.split('-')).lean();
-    identificacionRiesgo.map(element => {
-        element = removeNullValues(element)
-    })
-    res.json(identificacionRiesgo);
-};
+  const identificacionRiesgo = await IdentificacionRiesgo.find(
+    {
+      idCampus: req.params.idCampus,
+    },
+    req.params.keys == 'undefined' ? null : req.params.keys.split('-'),
+  ).lean()
+  identificacionRiesgo.map(element => {
+    element = removeNullValues(element)
+  })
+  res.json(identificacionRiesgo)
+}
 
 //Funciona bien
 identificacionRiesgoCtrl.createIdentificacionRiesgo = async (req, res) => {
-    console.log(req.body);
-    const {
-        idCampus,
-        proceso,
-        riesgo,
-        tipo,
-        escenarioRiesgo,
-        activo,
-        amenaza,
-        descripcion,
-        causas,
-        consecuencias,
-        nivelImpacto
-    } = req.body;
+  console.log(req.body)
+  const {
+    idCampus,
+    proceso,
+    riesgo,
+    tipo,
+    escenarioRiesgo,
+    activo,
+    amenaza,
+    descripcion,
+    causas,
+    consecuencias,
+    nivelImpacto,
+  } = req.body
 
-    const identificacionRiesgo = new IdentificacionRiesgo({
-        idCampus,
-        proceso,
-        riesgo,
-        tipo,
-        escenarioRiesgo,
-        activo,
-        amenaza,
-        descripcion,
-        causas,
-        consecuencias,
-        nivelImpacto
-    });
-    await identificacionRiesgo.save();
+  const identificacionRiesgo = new IdentificacionRiesgo({
+    idCampus,
+    proceso,
+    riesgo,
+    tipo,
+    escenarioRiesgo,
+    activo,
+    amenaza,
+    descripcion,
+    causas,
+    consecuencias,
+    nivelImpacto,
+  })
+  await identificacionRiesgo.save()
 
-    res.json({
-        message: "Riesgo guardado"
-    });
-};
+  res.json({
+    message: 'Riesgo guardado',
+  })
+}
 
 identificacionRiesgoCtrl.updateIdentificacionRiesgo = async (req, res) => {
-    const identificacionRiesgo = await IdentificacionRiesgo.findById(req.params.idRiesgo);
-    for (let key in req.body) {
-        identificacionRiesgo[key] = req.body[key];
-    }
-    await identificacionRiesgo.save();
-    res.json({
-        message: "Riesgo actualizado"
-    });
+  const identificacionRiesgo = await IdentificacionRiesgo.findById(
+    req.params.idRiesgo,
+  )
+  for (let key in req.body) {
+    identificacionRiesgo[key] = req.body[key]
+  }
+  await identificacionRiesgo.save()
+  res.json({
+    message: 'Riesgo actualizado',
+  })
 }
 identificacionRiesgoCtrl.updateCausa = async (req, res) => {
-    const identificacionRiesgo = await IdentificacionRiesgo.findById(req.params.idRiesgo);
-    identificacionRiesgo.causas = req.body.causas
-    identificacionRiesgo.solidez = req.body.solidez
-    await identificacionRiesgo.save();
-    res.json({
-        message: "Causa actualizada"
-    });
+  const identificacionRiesgo = await IdentificacionRiesgo.findById(
+    req.params.idRiesgo,
+  )
+  identificacionRiesgo.causas = req.body.causas
+  identificacionRiesgo.solidez = req.body.solidez
+  await identificacionRiesgo.save()
+  res.json({
+    message: 'Causa actualizada',
+  })
 }
 
 identificacionRiesgoCtrl.valorarControles = async (req, res) => {
-    const identificacionRiesgo = await IdentificacionRiesgo.findById(req.params.idRiesgo);
-    identificacionRiesgo.disminuirImpacto = req.body.disminuirImpacto
-    identificacionRiesgo.disminuirProbabilidad = req.body.disminuirProbabilidad
-    await identificacionRiesgo.save();
-    res.json({
-        message: "Controles valorados"
-    });
+  const identificacionRiesgo = await IdentificacionRiesgo.findById(
+    req.params.idRiesgo,
+  )
+  identificacionRiesgo.disminuirImpacto = req.body.disminuirImpacto
+  identificacionRiesgo.disminuirProbabilidad = req.body.disminuirProbabilidad
+  await identificacionRiesgo.save()
+  res.json({
+    message: 'Controles valorados',
+  })
 }
 
 identificacionRiesgoCtrl.tratarRiesgo = async (req, res) => {
-    const identificacionRiesgo = await IdentificacionRiesgo.findById(req.params.idRiesgo);
-    const {
-        estado,
-        tipoCompartir,
-        actividadesCumplidas,
-        incidentes,
-        planContingencia,
-        acciones
-    } = req.body
-    const tratamiento = {
-        estado,
-        actividadesCumplidas,
-        incidentes,
-        planContingencia,
-        tipoCompartir
-    };
-    identificacionRiesgo.tratamiento = tratamiento
-    if (acciones) {
-        identificacionRiesgo.causas.map(element => {
-            element.accion = acciones.find(acc => {
-                return acc.idCausa == element._id
-            });
-        })
-    } else {
-        identificacionRiesgo.causas.map(element => {
-            element.accion = null
-        })
-    }
-    await identificacionRiesgo.save();
-    res.json({
-        message: "Riesgo tratado"
-    });
+  const identificacionRiesgo = await IdentificacionRiesgo.findById(
+    req.params.idRiesgo,
+  )
+  const {
+    estado,
+    tipoCompartir,
+    actividadesCumplidas,
+    incidentes,
+    planContingencia,
+    acciones,
+  } = req.body
+  const tratamiento = {
+    estado,
+    actividadesCumplidas,
+    incidentes,
+    planContingencia,
+    tipoCompartir,
+  }
+  identificacionRiesgo.tratamiento = tratamiento
+  if (acciones) {
+    identificacionRiesgo.causas.map(element => {
+      element.accion = acciones.find(acc => {
+        return acc.idCausa == element._id
+      })
+    })
+  } else {
+    identificacionRiesgo.causas.map(element => {
+      element.accion = null
+    })
+  }
+  await identificacionRiesgo.save()
+  res.json({
+    message: 'Riesgo tratado',
+  })
 }
 
-
 identificacionRiesgoCtrl.guardarProbabilidad = async (req, res) => {
-    const arrRiesgos = req.body.riesgos;
+  const arrRiesgos = req.body.riesgos
 
-    arrRiesgos.forEach(async element => {
+  arrRiesgos.forEach(async element => {
+    const riesgo = await IdentificacionRiesgo.findById(element.idRiesgo)
+    for (let key in element) {
+      riesgo[key] = element[key]
+    }
+    await riesgo.save()
+  })
+  res.json({
+    message: 'Probabilidades actualizadas',
+  })
+}
 
-        const riesgo = await IdentificacionRiesgo.findById(element.id);
-        for (let key in element) {
-            riesgo[key] = element[key];
-        }
-        await riesgo.save();
-    });
-    res.json({
-        message: "Probabilidades actualizadas"
-    });
-};
-
-module.exports = identificacionRiesgoCtrl;
+module.exports = identificacionRiesgoCtrl
